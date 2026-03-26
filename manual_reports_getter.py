30901f813caf41462835b1635bf225e6bf08e101
Eloi Vergé <eloi.verge@euroleague.net>
2026-03-26 16:05:40 +0100

    reports que faltaven d'oscar

--- manual_reports_getter.py
+++ manual_reports_getter.py
@@ -6,7 +6,6 @@ from correction import Correction
 from dataclasses import asdict
 import sqlite3
 from datetime import time, datetime
-import pandas as pd
 
 def update_game(conn, game_code, game_data):
     cursor = conn.cursor()
